d16dc516af5dc7eadfce2aaaeb38786a1d10abab
officialrajdeepsingh <radewala.2@gmail.com>
2020-08-01 16:48:12 +0530

    working start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a96ee12af7c4218e8a2088339a26562'),
+    },
+  },
+});
